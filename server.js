03f794b3585462a8b01d6545de961492e64d353a
bongkoch-ek <ekp.bongkoch@gmail.com>
2024-10-01 10:15:33 +0700

    1st

---
+++ server.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 8800
+app.listen(port, () => console.log("server run on port", port))
